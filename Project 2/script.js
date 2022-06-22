0311b5ce66aa582cdfb335c1a515a5fe670520b1
anas0912 <39875135+anas0912@users.noreply.github.com>
2022-06-22 17:18:25 +0500

    js updated

--- Project 2/script.js
+++ Project 2/script.js
@@ -8,13 +8,52 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 
-const ticketPrice = +movieSelect.value;
 
+
+let ticketPrice = +movieSelect.value;
+
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const seatsIndex = [...selectedSeats].map(seat => [...seats].indexOf(seat));
+    const selectedSeatsCount = selectedSeats.length;
+    count.innerText = selectedSeatsCount;
+    total.innerText = selectedSeatsCount * ticketPrice;
+    localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex))
+}
+
+
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
+// get date from localstorage and populate UI
+
+
+function populateUI() {
+    const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+    if (selectedSeats !== null) {
+
+    }
+}
+
+// Event listner for for container to check for click on seats
 container.addEventListener('click', e => {
 
     if (e.target.classList.contains('seat') &&
         !e.target.classList.contains('occupied')
     ) {
-        e.target.classList.toggle('selected')
+        e.target.classList.toggle('selected');
+        updateSelectedCount();
     }
 })
+
+// Event listner for movie select
+movieSelect.addEventListener('change', e => {
+
+        ticketPrice = +e.target.value;
+        setMovieData(e.target.selectedIndex, e.target.value);
+        updateSelectedCount();
+    }
+
+)
