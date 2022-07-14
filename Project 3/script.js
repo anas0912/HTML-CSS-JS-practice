0a2886c930c47639a851861eb379c79ae138b10f
anas0912 <39875135+anas0912@users.noreply.github.com>
2022-07-14 16:23:04 +0500

    js

---
+++ Project 3/script.js
@@ -0,0 +1,9 @@
+//  Get DOM Elements For JS code
+const video = document.getElementById('video');
+const play = document.getElementById('play');
+const stop = document.getElementById('stop');
+const progress = document.getElementById('progress');
+const timestamp = document.getElementById('timestamp');
+
+// Event Listeners
+// 1.
