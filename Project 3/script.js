11399d7edd9107bdff0cb265b36ff30981763aae
anas0912 <39875135+anas0912@users.noreply.github.com>
2022-07-21 18:17:48 +0500

    Update script.js
    event listeners and functions added

--- Project 3/script.js
+++ Project 3/script.js
@@ -5,5 +5,63 @@ const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
 
+
+//  Create function for clicking on video
+
+function toggleVideoStatus() {
+    return true;
+}
+
+
+
+//  Create function for play/pause icon
+
+function updatePlayIcon() {
+    return true;
+}
+
+
+
+//  Create function to update the progress
+
+function updateProgress() {
+    return true;
+}
+
+// Create function to stop the video
+
+function stopVideo() {
+    return true;
+}
+
+
+// Create function to update the progress using slide bar
+
+function setVideoProgress() {
+    return true;
+}
+
+
+
+
 // Event Listeners
-// 1. For clicking on the video
+// 1. For Video player
+
+video.addEventListener('click', toggleVideoStatus);
+video.addEventListener('pause', updatePlayIcon);
+video.addEventListener('play', updatePlayIcon);
+video.addEventListener('timeupdate', updateProgress);
+
+
+// 2. Event Listener for play button
+
+play.addEventListener('click', toggleVideoStatus);
+
+
+// 3. Event Listener for stop button
+
+stop.addEventListener('click', stopVideo);
+
+// 4. Event Listener for progress bar
+
+progress.addEventListener('change', setVideoProgress);
