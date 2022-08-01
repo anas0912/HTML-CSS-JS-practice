1db505584fd444146fb154f5ba0d649a50984ffa
anas0912 <39875135+anas0912@users.noreply.github.com>
2022-08-01 16:51:52 +0500

    js
    j

--- Project 3/script.js
+++ Project 3/script.js
@@ -1,6 +1,7 @@
 //  Get DOM Elements For JS code
 const video = document.getElementById('video');
 const play = document.getElementById('play');
+// const pause = document.getElementById('pause');
 const stop = document.getElementById('stop');
 const progress = document.getElementById('progress');
 const timestamp = document.getElementById('timestamp');
@@ -9,7 +10,11 @@ const timestamp = document.getElementById('timestamp');
 //  Create function for clicking on video
 
 function toggleVideoStatus() {
-    return true;
+    if (video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
 }
 
 
@@ -17,7 +22,11 @@ function toggleVideoStatus() {
 //  Create function for play/pause icon
 
 function updatePlayIcon() {
-    return true;
+    if (video.paused) {
+        play.innerHTML = '<i class="fa-solid fa-play"></i>'
+    } else {
+        play.innerHTML = '<i class="fa-solid fa-pause"></i>'
+    }
 }
 
 
@@ -25,13 +34,14 @@ function updatePlayIcon() {
 //  Create function to update the progress
 
 function updateProgress() {
-    return true;
+    console.log(video.duration);
 }
 
 // Create function to stop the video
 
 function stopVideo() {
-    return true;
+    video.currentTime = 0;
+    video.pause();
 }
 
 
